591167569977f314f878b90d2b6df80d4e6b2096
MarinaIgnat <9702m@mail.ru>
2024-07-01 19:32:40 +0700

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,10 @@
+my_string = input('В каком городе Вы проживаете? ')
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print('В каком городе Вы проживаете? '.replace(' ', ''))
+print(my_string[:1])
+print(my_string[-1:])
+
+
+
